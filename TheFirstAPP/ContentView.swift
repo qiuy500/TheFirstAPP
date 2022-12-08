c2f003b24c6a65bf0bc6a456396440c8dee22f74
qiuy500 <qiuy500@gmail.com>
2022-12-08 09:19:20 +0800

    Initial Commit

---
+++ TheFirstAPP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TheFirstAPP
+//
+//  Created by 暨大附中２ on 2022/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheFirstAPP/TheFirstAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFirstAPPApp.swift
+//  TheFirstAPP
+//
+//  Created by 暨大附中２ on 2022/12/8.
+//
+
+import SwiftUI
+
+@main
+struct TheFirstAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
