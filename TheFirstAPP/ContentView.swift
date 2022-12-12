e7e6268bb5ec85f4e42de24b5d8065bd6d27846f
qiuy500 <qiuy500@gmail.com>
2022-12-12 11:52:15 +0800

    text

--- TheFirstAPP/ContentView.swift
+++ TheFirstAPP/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("text")
+            Text("text ")
         }
         .padding()
     }
