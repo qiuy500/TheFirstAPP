883564280cf281d5cf875a11bc1b4b005e1e3a99
grievouslady123 <qiuy500@gmail.com>
2022-12-23 11:20:43 +0800

    tomato

---
+++ TheFirstAPP/Mode/ScheduleList.swift
@@ -0,0 +1,38 @@
+//
+//  ScheduleList.swift
+//  TheFirstAPP
+//
+//  Created by 暨大附中１ on 2022/12/25.
+//
+
+import SwiftUI
+
+struct ScheduleList: View {
+    @State private var backgroundColor = Color.white
+    var ListNumber : [String] = ["1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"]
+    var body: some View {
+        VStack{
+            Button{
+                
+            }label: {
+                
+            }
+            
+            
+            
+            List{
+                
+                ForEach( 0...23,id:\.self){
+                    Text(ListNumber[$0 % ListNumber.count])
+                }
+            }
+        }
+    }
+}
+
+struct ScheduleList_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleList()
+    }
+}
+
--- TheFirstAPP/Mode/ToMaToView.swift
+++ TheFirstAPP/Mode/ToMaToView.swift
@@ -15,8 +15,10 @@ struct ToMaToView: View {
     @State var x = 0
     @State var y = 0
     @State var heidht:CGFloat = 50
-    @State var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect() // 建立一個計時器
+    @State var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect() // 建立一個計時器
+    @State private var scale: Double = 300
     @State var audioPlayer: AVAudioPlayer!
+    @State private var value: Double = 600
     let url = Bundle.main.url(forResource: "番茄鈴聲", withExtension: "mp3")
     @State var soundswitch = false
     var body: some View {
@@ -24,15 +26,27 @@ struct ToMaToView: View {
         VStack {
             // 定義一個文本標籤，用於顯示定時器的剩餘時間
             //Text("\(soundswitch)")
-            Text("x = \(x)")
-            Text("y = \(y)")
-            Image("tomato")
+            //Text("x = \(x)")
+            //Text("y = \(y)")
+            let scaleString = scale.formatted(.number.precision(.fractionLength(2)))
+            Text(scaleString)
+            
+            /*Image("tomato")
                 .frame(alignment: .leading)
-                .padding()
-            Image(systemName: "square.fill")
+                .padding()*/
+                Circle()
+                    .trim(from: 0, to: CGFloat(scale))
+                    .stroke(Color.red, style: StrokeStyle(lineWidth: 20, lineCap: .round))
+                    .frame(width: 200, height: 200)
+
+                Slider(value: $scale, in: 1...600)
+            
+            //Slider(value: $scale, in: 1...300)
+            /*Image(systemName: "square.fill")
                 .resizable()
+                //.scaleEffect(scale)
                 .foregroundColor(Color.red)
-                .frame(width: 10,height: heidht)
+                .frame(width: 10,height: scale)*/
             Text("剩餘時間：\(timeRemaining / 60) 分鐘\(timeRemaining - (timeRemaining / 60 * 60))秒")
                 .font(.largeTitle)
                 .padding(.bottom, 50)
@@ -40,20 +54,21 @@ struct ToMaToView: View {
                     if isRunning{
                         if self.timeRemaining > 0 { // 如果計時器還沒有結束
                             self.timeRemaining -= 1 // 將剩餘時間減一秒
-                            if heidht > 0{
-                                heidht -= 5
+                            if scale > 0{
+                                scale -= 1
                             }
                         }else{//計時器結束
                             self.isRunning = false
-                            soundswitch = true
+                            soundswitch = true //開叫
                         }
                     }else{
-                        timeRemaining = min * 60 + sen
+                        //timeRemaining = min * 60 + sen
+                        timeRemaining = Int(scale)
                         heidht = CGFloat(self.timeRemaining)
                     }
-                    if timeRemaining == 0{
-                        soundswitch = true
-                    }
+                   /* if timeRemaining == 0{
+                        soundswitch = true //開叫
+                    }*/
                     if soundswitch == true{
                         do{
                             audioPlayer = try AVAudioPlayer(contentsOf: url!)
@@ -65,14 +80,25 @@ struct ToMaToView: View {
                         }
                     }
                     // 定義一個按鈕，用於控制定時器的運行
-                    if x == 0 || x != 0{
+                    /*if x == 0 || x != 0{
                         if x > y {
-                            y += 1
+                            y += 3
                         }else{
-                            x += 1
+                            x += 3
                         }
-                    }
+                    }*/
                 }
+            Button(action: {
+                if isRunning {
+                    timeRemaining = Int(scale)
+                }else{
+                    timeRemaining = Int(scale)
+
+                }
+            }){
+                Text("reset")
+            }.font(.custom("", size: 50))
+            
             Button(action: {
                 // 點擊按鈕時，切換定時器的運行狀態
                 self.isRunning.toggle()
@@ -92,21 +118,21 @@ struct ToMaToView: View {
                     }
                 }
 
-            }
-            .font(.custom("", size: 50))
+            }.font(.custom("", size: 50))
             
             // 定義一個文本框，用於輸入定時器的時間
-            HStack {
+            /*HStack {
                 Text("輸入分鐘")
                 TextField("輸入分鐘", value: $min, formatter: NumberFormatter())
                     .keyboardType(.numberPad)
                 Text("輸入秒")
                 TextField("輸入秒", value: $sen, formatter: NumberFormatter())
                     .keyboardType(.numberPad)
-            }
+            }*/
+
             
-           
             }
+        
         }
     }
 
