0b977a1597f66d9b746143960dacb5dcf87c6e61
grievouslady123 <qiuy500@gmail.com>
2023-01-03 11:10:31 +0800

    q

---
+++ TheFirstAPP/LoagImg/AsyncImage.swift
@@ -0,0 +1,39 @@
+//
+//  AsyncImage.swift
+//  auth_demo
+//
+//  Created by kai wen chen on 2022/12/28.
+//
+
+import SwiftUI
+
+struct AsyncImage<Placeholder: View>: View {
+    @StateObject private var loader: ImageLoader
+    private let placeholder: Placeholder
+    private let image: (UIImage) -> Image
+    
+    init(
+        url: URL,
+        @ViewBuilder placeholder: () -> Placeholder,
+        @ViewBuilder image: @escaping (UIImage) -> Image = Image.init(uiImage:)
+    ) {
+        self.placeholder = placeholder()
+        self.image = image
+        _loader = StateObject(wrappedValue: ImageLoader(url: url, cache: Environment(\.imageCache).wrappedValue))
+    }
+    
+    var body: some View {
+        content
+            .onAppear(perform: loader.load)
+    }
+    
+    private var content: some View {
+        Group {
+            if loader.image != nil {
+                image(loader.image!)
+            } else {
+                placeholder
+            }
+        }
+    }
+}
---
+++ TheFirstAPP/Mode/ScheduleList2.swift
@@ -0,0 +1,27 @@
+//
+//  ScheduleList2.swift
+//  TheFirstAPP
+//
+//  Created by 暨大附中１ on 2023/1/2.
+//
+
+import SwiftUI
+
+struct ScheduleList2: View {
+    var body: some View {
+        VStack{
+            List{
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            }
+        }
+    }
+}
+struct ScheduleList2_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleList2()
+    }
+}
